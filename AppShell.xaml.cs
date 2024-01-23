7b698d0fb712b8e91408da5966742a2b01824986
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2024-01-23 08:45:22 -0500

    FirstCommit
    Example with we interact with the OS and your Themes

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TemasDelSistema
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace TemasDelSistema
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Application.Current.UserAppTheme = AppTheme.Dark;
+
+            AppTheme temaActual = Application.Current.RequestedTheme;
+
+            Label label = new Label();
+            label.SetAppThemeColor(Label.TextColorProperty, Colors.Purple, Colors.White);
+            label.Text = $"Este texto hace referencia al que esta construido con C#, el tema actual del sistema es:  {temaActual}";
+
+            Image image = new Image();
+            image.SetAppTheme<FileImageSource>(Image.SourceProperty, "darkimg.png", "lightimg.png");
+            image.WidthRequest = 100;
+            image.HeightRequest = 100;
+
+            stack.Children.Add(label);
+            stack.Children.Add(image);
+
+            Application.Current.RequestedThemeChanged += (s, a) =>
+            {
+                Application.Current.MainPage.DisplayAlert("Alerta", "Cambio el tema", "Ok");
+            };
+        }
+
+        
+    }
+
+}
